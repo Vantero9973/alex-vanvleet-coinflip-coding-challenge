cbc000ca3187a4e1547297988d1d11b9daa29d8f
Alex Van Vleet <alexvan73@yahoo.com>
2025-01-28 23:14:13 +0700

    Fix: Reset search query and results when reopening modal

--- components/SearchModal.tsx
+++ components/SearchModal.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { useQuery } from "@tanstack/react-query";
 import { useRouter } from "next/navigation";
 import {
@@ -43,6 +43,12 @@ export default function SearchModal({
       : data?.data.filter((asset: Asset) =>
           asset.name.toLowerCase().includes(query.toLowerCase())
         ) || [];
+
+  useEffect(() => {
+    if (open) {
+      setQuery("");
+    }
+  }, [open]);
 
   return (
     <Dialog
