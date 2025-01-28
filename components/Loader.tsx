7d37f1582381ec72a23a4e8f693c43df17ec80a9
Alex Van Vleet <alexvan73@yahoo.com>
2025-01-28 21:12:41 +0700

    Change loader color

--- components/Loader.tsx
+++ components/Loader.tsx
@@ -5,7 +5,7 @@ import React from "react";
 export default function Loader(): JSX.Element {
   return (
     <div className="fixed inset-0 flex items-center justify-center z-50">
-      <div className="h-24 w-24 border-8 border-t-8 border-[#e7bb43]/25 rounded-full border-t-[#e7bb43] animate-spin" />
+      <div className="h-24 w-24 border-8 border-t-8 border-zinc-300 rounded-full border-t-zinc-600 animate-spin"></div>
     </div>
   );
 }
